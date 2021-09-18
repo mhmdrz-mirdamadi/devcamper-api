b0e9b2be7ce84f7411ea160dd04f543c477be89a
mhmdrz <smrm1381@yahoo.com>
2021-09-18 22:30:26 +0430

    Class error added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc        Get all bootcamps
@@ -19,13 +20,13 @@ exports.getBootcamps = async (req, res, next) => {
 exports.getBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findById(req.params.id);
-        if (!bootcamp)
-            return res.status(400).json({ success: false });
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamo not found with id of ${req.params.id}`, 404));
+        }
         res.status(200).json({ success: true, data: bootcamp });
     }
     catch (err) {
-        //res.status(400).json({ success: false });
-        next(err);
+        next(new ErrorResponse(`Bootcamo not found with id of ${req.params.id}`, 404));
     }
 };
 
