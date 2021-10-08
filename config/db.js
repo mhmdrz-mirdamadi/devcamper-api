9d11ce275a12a62cedb1050e9b37463fbb573f5c
mhmdrz <smrm1381@yahoo.com>
2021-10-08 10:44:39 +0330

    Database seeder for bootcamps

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`
         .cyan.underline.bold);
 };
