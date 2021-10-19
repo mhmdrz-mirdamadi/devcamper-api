7c842f9f94d494aa4c54b89c07bf1827a540ffe8
mhmdrz <smrm1381@yahoo.com>
2021-10-19 17:20:10 +0330

    Populate virtuals casacade delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
     // Finding resource
-    query = Bootcamp.find(JSON.parse(queryStr));
+    query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
     // Select Fields
     if (req.query.select) {
@@ -72,7 +72,12 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
     res
         .status(200)
-        .json({ success: true, count: bootcamps.length, pagination, data: bootcamps });
+        .json({
+            success: true,
+            count: bootcamps.length,
+            pagination,
+            data: bootcamps
+        });
 });
 
 // @desc        Get single bootcamp
@@ -83,7 +88,13 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamo not found with id of ${req.params.id}`, 404));
     }
-    res.status(200).json({ success: true, data: bootcamp });
+
+    res
+        .status(200)
+        .json({
+            success: true,
+            data: bootcamp
+        });
 });
 
 // @desc        Create new bootcamp
@@ -91,7 +102,13 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @access      Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({ success: true, data: bootcamp });
+
+    res
+        .status(201)
+        .json({
+            success: true,
+            data: bootcamp
+        });
 });
 
 // @desc        Update bootcamp
@@ -105,17 +122,30 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
         });
     if (!bootcamp)
         return next(new ErrorResponse(`Bootcamo not found with id of ${req.params.id}`, 404));
-    res.status(200).json({ success: true, data: bootcamp });
+
+    res
+        .status(200)
+        .json({
+            success: true,
+            data: bootcamp
+        });
 });
 
 // @desc        Delete bootcamp
 // @route       DELETE /api/v1/bootcamps/:id
 // @access      Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp)
         return next(new ErrorResponse(`Bootcamo not found with id of ${req.params.id}`, 404));
-    res.status(200).json({ success: true, data: {} });
+    bootcamp.remove();
+
+    res
+        .status(200)
+        .json({
+            success: true,
+            data: {}
+        });
 });
 
 // @desc        Get bootcamps within a radius
@@ -138,9 +168,11 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
         location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
     });
 
-    res.status(200).json({
-        success: true,
-        count: bootcamps.length,
-        data: bootcamps
-    })
+    res
+        .status(200)
+        .json({
+            success: true,
+            count: bootcamps.length,
+            data: bootcamps
+        })
 });
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -121,7 +121,10 @@ const BootcampSchema = new mongoose.Schema(
             type: Date,
             default: Date.now
         }
-    });
+    }, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
 
 // Create bootcamp slug from the name
 BootcampSchema.pre('save', function (next) {
@@ -146,6 +149,21 @@ BootcampSchema.pre('save', async function (next) {
     // Do not save address in DB
     this.address = undefined;
     next();
+});
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+    console.log(`${this._id} removed`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+});
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
